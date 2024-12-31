afb6b4072238021012d93293163137ac5ad09107
max2002 <chiayang.lu@gmail.com>
2024-12-31 10:59:44 +0800

    241231 test mount EDS agent

--- _EC2.tf
+++ _EC2.tf
@@ -17,7 +17,7 @@ resource "aws_security_group" "public_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress  {
+  egress {
     from_port   = 0
     to_port     = 0
     protocol    = -1
@@ -36,13 +36,13 @@ resource "aws_security_group" "private_sg" {
   description = "Allow traffic from Public SG"
 
   ingress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    security_groups  = [aws_security_group.public_sg.id]
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    security_groups = [aws_security_group.public_sg.id]
   }
-  
-  egress  {
+
+  egress {
     from_port   = 0
     to_port     = 0
     protocol    = -1
@@ -56,11 +56,11 @@ resource "aws_security_group" "private_sg" {
 
 # public ec2-1
 resource "aws_instance" "public_ec2_1" {
-  ami                  = var.EC2_AMI.x86ID  
-  instance_type        = var.EC2_size.t3micro
-  subnet_id            = aws_subnet.public_subnet_1.id
-  vpc_security_group_ids = [aws_security_group.public_sg.id]  # 使用 security group ID
-  
+  ami                    = var.EC2_AMI.win_2016_corebase
+  instance_type          = var.EC2_size.t3small
+  subnet_id              = aws_subnet.public_subnet_1.id
+  vpc_security_group_ids = [aws_security_group.public_sg.id] # 使用 security group ID
+
   # attach iam role
   iam_instance_profile = aws_iam_instance_profile.ec2_admin_instance_profile.name
 
@@ -71,11 +71,11 @@ resource "aws_instance" "public_ec2_1" {
 
 # public ec2-2
 resource "aws_instance" "public_ec2_2" {
-  ami                  = var.EC2_AMI.x86ID  
-  instance_type        = var.EC2_size.t3micro
-  subnet_id            = aws_subnet.public_subnet_2.id
-  vpc_security_group_ids = [aws_security_group.public_sg.id]  # 使用 security group ID
-  
+  ami                    = var.EC2_AMI.win_2016_corebase
+  instance_type          = var.EC2_size.t3small
+  subnet_id              = aws_subnet.public_subnet_2.id
+  vpc_security_group_ids = [aws_security_group.public_sg.id] # 使用 security group ID
+
   # attach iam role
   iam_instance_profile = aws_iam_instance_profile.ec2_admin_instance_profile.name
 
@@ -86,11 +86,11 @@ resource "aws_instance" "public_ec2_2" {
 
 # private ec2-1
 resource "aws_instance" "private_ec2_1" {
-  ami                  = var.EC2_AMI.x86ID  
-  instance_type        = var.EC2_size.t3micro
-  subnet_id            = aws_subnet.private_subnet_1.id
-  vpc_security_group_ids = [aws_security_group.private_sg.id]  # 使用 security group ID
-  
+  ami                    = var.EC2_AMI.x86ID
+  instance_type          = var.EC2_size.t3micro
+  subnet_id              = aws_subnet.private_subnet_1.id
+  vpc_security_group_ids = [aws_security_group.private_sg.id] # 使用 security group ID
+
   # attach iam role
   iam_instance_profile = aws_iam_instance_profile.ec2_admin_instance_profile.name
 
@@ -101,11 +101,11 @@ resource "aws_instance" "private_ec2_1" {
 
 # private ec2-2
 resource "aws_instance" "private_ec2_2" {
-  ami                  = var.EC2_AMI.x86ID  
-  instance_type        = var.EC2_size.t3micro
-  subnet_id            = aws_subnet.private_subnet_2.id
-  vpc_security_group_ids = [aws_security_group.private_sg.id]  # 使用 security group ID
-  
+  ami                    = var.EC2_AMI.x86ID
+  instance_type          = var.EC2_size.t3micro
+  subnet_id              = aws_subnet.private_subnet_2.id
+  vpc_security_group_ids = [aws_security_group.private_sg.id] # 使用 security group ID
+
   # attach iam role
   iam_instance_profile = aws_iam_instance_profile.ec2_admin_instance_profile.name
 
--- _S3.tf
+++ _S3.tf
@@ -2,5 +2,6 @@
 resource "aws_s3_bucket" "my_bucket" {
   count  = length(var.bucket_names)
   bucket = var.bucket_names[count.index]
+  force_destroy = true
   tags = var.tags  # select the tag in variable
 }
